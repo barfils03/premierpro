19632788c9c16f7c6971252bbcaf2656fa30b758
barfils03 <dagbamefranck02@gmail.com>
2025-02-17 23:57:05 -0800

    commit

--- week02/Develop05/obj/Debug/net8.0/Develop05.AssemblyInfo.cs
+++ week02/Develop05/obj/Debug/net8.0/Develop05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Develop05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5130418377b00bfc4041611dfbf3b8d3be33fcc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Develop05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Develop05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
